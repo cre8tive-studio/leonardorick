6e718dac7f0cc5dc13786f914bace692afff0fa1
Leonardo Rick <leonardo.rick@unbabel.com>
2023-06-13 10:42:15 +0100

    try another approach on nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { DEFAULT_HEAD } from './utils/analytics/head';
-const { VUE_APP_ENVIRONMENT: environment, NITROPACK_PRESET: preset } = process.env;
-
+const { NITROPACK_PRESET: preset } = process.env;
+console.log('preset', preset);
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.scss'],
@@ -12,18 +12,18 @@ export default defineNuxtConfig({
   // todo think about a better approach until cloudflare supports runtimeconfig
   // https://github.com/unjs/nitro/issues/272
   // https://nitro.unjs.io/deploy/providers/cloudflare
-  runtimeConfig: {
-    // Private config that is only available on the server
-    VUE_APP_SANITY_PROJECT_ID: process.env.VUE_APP_SANITY_PROJECT_ID,
-    VUE_APP_SANITY_CLIENT_TOKEN: process.env.VUE_APP_SANITY_CLIENT_TOKEN,
-    VUE_APP_SANITY_GRAPHQL_URL: process.env.VUE_APP_SANITY_GRAPHQL_URL,
-    // Config within public will be also exposed to the client
-    public: {
-      VUE_APP_ENVIRONMENT: process.env.VUE_APP_ENVIRONMENT,
-      VUE_APP_BASE_URL: process.env.VUE_APP_BASE_URL,
-      environment,
-    },
-  },
+  // runtimeConfig: {
+  //   // Private config that is only available on the server
+  //   VUE_APP_SANITY_PROJECT_ID: process.env.VUE_APP_SANITY_PROJECT_ID,
+  //   VUE_APP_SANITY_CLIENT_TOKEN: process.env.VUE_APP_SANITY_CLIENT_TOKEN,
+  //   VUE_APP_SANITY_GRAPHQL_URL: process.env.VUE_APP_SANITY_GRAPHQL_URL,
+  //   // Config within public will be also exposed to the client
+  //   public: {
+  //     VUE_APP_ENVIRONMENT: process.env.VUE_APP_ENVIRONMENT,
+  //     VUE_APP_BASE_URL: process.env.VUE_APP_BASE_URL,
+  //     environment,
+  //   },
+  // },
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
@@ -42,10 +42,6 @@ export default defineNuxtConfig({
   // pre generate other routes as well
   nitro: {
     ...(preset ? { preset } : {}),
-    output: {
-      serverDir: 'dist/server',
-      publicDir: 'dist/client',
-    },
     prerender: {
       crawlLinks: true, // use true when you want all routes to be pre-rendered
       // this two options might be still experimental so check the need of
