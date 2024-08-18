e7feab927e432f151e03254a03c166ce12e3caa5
Leonardo Rick <leonardo.rick@unbabel.com>
2024-08-18 19:47:50 +0100

    add missing properties on animationStore

--- store/animation.ts
+++ store/animation.ts
@@ -3,23 +3,31 @@ import type { Fluid } from '~/components/FluidBackground/fluid';
 interface animationStoreModel {
   isEnteringAnimationFinished: boolean;
   isLRModelLoaded: boolean;
+  isOverlayCompleteHidden: boolean;
   loadingProgress: number;
   loadingTotal: number;
   fluid: Fluid | null;
   fluidCanvas: HTMLCanvasElement | undefined;
   logoCanvas: HTMLCanvasElement | undefined;
   scrollLayout: HTMLElement | undefined;
+  loadingBarRef: HTMLDivElement | undefined;
+  logoOverlayRef: HTMLDivElement | undefined;
+  cubeLoaderContainerRef: HTMLDivElement | undefined;
 }
 export const useAnimationStore = defineStore('animationStore', () => {
   const state = reactive<animationStoreModel>({
     isEnteringAnimationFinished: false,
     isLRModelLoaded: false,
+    isOverlayCompleteHidden: false,
     loadingProgress: 0,
     loadingTotal: 0,
     fluid: null,
     fluidCanvas: undefined,
     logoCanvas: undefined,
     scrollLayout: undefined,
+    loadingBarRef: undefined,
+    logoOverlayRef: undefined,
+    cubeLoaderContainerRef: undefined,
   });
 
   const changeScrollLayoutOverflow = (overflow: '' | 'hidden') => {
