6131c51e72eea4cf7fa9bef9d3af273c68d95935
Leonardo Rick <leonardo.rick@unbabel.com>
2024-08-28 15:58:59 +0100

    remove posthog if configuration is not proper (env variables

--- plugins/posthog.client.ts
+++ plugins/posthog.client.ts
@@ -1,7 +1,18 @@
 import { posthog } from 'posthog-js';
 import { defineNuxtPlugin } from '#app';
 export default defineNuxtPlugin(() => {
-  const { publicKey, host } = useRuntimeConfig().public.posthog;
+  const c = useRuntimeConfig();
+  const { publicKey, host } = c.public.posthog;
+  const env = c.public.environment;
+
+  if (!publicKey || !host) {
+    if (isProduction(env)) {
+      // eslint-disable-next-line no-console
+      console.warn('Posthog not intialzed, missing configuration properties (key or url)');
+    }
+    return;
+  }
+
   const posthogClient = posthog.init(publicKey, {
     api_host: host,
     loaded: (_pthog) => {
