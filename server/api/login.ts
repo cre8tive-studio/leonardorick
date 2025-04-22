c1d3c729bf5ee405d36435dda1d0aa376760eb2b
Leonardo Rick <leonardo.rick@qonto.com>
2025-04-22 16:26:12 +0100

    debug-cloudflare-error-1

--- server/api/login.ts
+++ server/api/login.ts
@@ -5,6 +5,7 @@ import useServerAppwrite from '~/composables/use-server-appwrite';
 const { users, queryAllowedEmail, getAuthUserWithEmail, getSettings } = useServerAppwrite();
 
 export default defineEventHandler(async (event) => {
+  console.log('Request method:', event.node.req.method);
   const { email } = await readBody(event);
 
   try {
